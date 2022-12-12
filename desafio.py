aaaf1fdc01b40b7825873115d70dcbe52e8c65b9
LucasTalen <103639813+LucasTalen@users.noreply.github.com>
2022-12-12 20:30:22 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,5 @@
+print(f"{5*0.6-1/2*9.81*0.6**2}")
+print((5*0.6)-1/(2*9.81*(0.6**2)))
+print((5*0.6)-((1/2)*9.81*(0.8**2)))
+#Infelizmente não conseguir identificar qual seria a ordem de cálculo coreto
+#por isso deixei as 3 principais resultados que poderia da sobre esse calculo.
